ca837d3e97ad77624fab3b2c9a146a24f15e3f0e
delasimensah <mensadelasi@gmail.com>
2021-10-31 11:50:17 +0000

    initial push

---
+++ README.md
@@ -0,0 +1,1 @@
+## Digi Mobile App Landing Website
---
+++ src/sections/feature.js
@@ -0,0 +1,106 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "components/section-header";
+import FeatureCard from "components/feature-card.js";
+import Smart from "assets/feature/smart.svg";
+import Winner from "assets/feature/winner.svg";
+import Cloud from "assets/feature/cloud.svg";
+import Setting from "assets/feature/setting.svg";
+import Design from "assets/feature/design.svg";
+import Chat from "assets/feature/chat.svg";
+
+const data = [
+  {
+    id: 1,
+    imgSrc: Smart,
+    altText: "Smart",
+    title: "Smart Features",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+  {
+    id: 2,
+    imgSrc: Winner,
+    altText: "Performance",
+    title: "Fast Performance",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+  {
+    id: 3,
+    imgSrc: Cloud,
+    altText: "Content",
+    title: "Unlimited Content",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+  {
+    id: 4,
+    imgSrc: Setting,
+    altText: "Customization",
+    title: "Unlimited Customization",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+  {
+    id: 5,
+    imgSrc: Design,
+    altText: "Productivity",
+    title: "Boost Productivity",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+  {
+    id: 6,
+    imgSrc: Chat,
+    altText: "Support",
+    title: "Customer Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+  },
+];
+
+export default function Feature() {
+  return (
+    <section sx={{ variant: "section.feature" }}>
+      <Container>
+        <SectionHeader
+          slogan="Quality features"
+          title="Meet exciting feature of app"
+        />
+
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <FeatureCard
+              key={item.id}
+              src={item.imgSrc}
+              alt={item.title}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  grid: {
+    pt: [0, null, null, null, null, null, null, 3],
+    gridGap: [
+      "37px 0",
+      null,
+      "45px 30px",
+      null,
+      "50px 30px",
+      null,
+      null,
+      "90px 70px",
+    ],
+    width: ["100%", "80%", "100%"],
+    mx: "auto",
+    gridTemplateColumns: [
+      "repeat(1,1fr)",
+      null,
+      "repeat(2,1fr)",
+      null,
+      "repeat(3,1fr)",
+    ],
+  },
+};
